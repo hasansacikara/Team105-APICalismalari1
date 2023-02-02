4b10774fd25c2e6242d871ea2418dbc8c5d0a832
hasan.sacikara <114678377+hasansacikara@users.noreply.github.com>
2023-02-02 22:37:08 +0300

    5. gün sonu

--- src/test/java/testData/TestDataDummy.java
+++ src/test/java/testData/TestDataDummy.java
@@ -2,7 +2,12 @@ package testData;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 public class TestDataDummy {
+
+    public int basariliStatusCode;
+    public int contenrType;
 
     /*
     {
@@ -18,27 +23,51 @@ public class TestDataDummy {
     }
      */
 
-    public JSONObject innerJSONBody (){
+    public JSONObject innerJSONBody() {
 
         JSONObject innerBody = new JSONObject();
 
         innerBody.put("id", 3);
-        innerBody.put("employee_name","Ashton Cox");
-        innerBody.put("employee_salary",86000);
-        innerBody.put("employee_age",66);
-        innerBody.put("profile_image","");
+        innerBody.put("employee_name", "Ashton Cox");
+        innerBody.put("employee_salary", 86000);
+        innerBody.put("employee_age", 66);
+        innerBody.put("profile_image", "");
 
         return innerBody;
     }
-    public JSONObject expectedBodyOlusturJson (){
+
+    public JSONObject expectedBodyOlusturJson() {
 
         JSONObject expBody = new JSONObject();
 
-        expBody.put("status","success");
-        expBody.put("message","Successfully! Record has been fetched.");
-        expBody.put("data",innerJSONBody());
+        expBody.put("status", "success");
+        expBody.put("message", "Successfully! Record has been fetched.");
+        expBody.put("data", innerJSONBody());
 
         return expBody;
     }
 
+    public HashMap innerBodyOlusturMap() {
+
+        HashMap<String, Object> innerBodyMap = new HashMap<>();
+
+        innerBodyMap.put("id", 3.0);
+        innerBodyMap.put("employee_name", "Ashton Cox");
+        innerBodyMap.put("employee_salary", 86000);
+        innerBodyMap.put("employee_age", 66);
+        innerBodyMap.put("profile_image", "");
+
+        return innerBodyMap;
+    }
+
+    public HashMap expectedBodyOlusturMap() {
+
+        HashMap<String, Object> expBody = new HashMap<>();
+
+        expBody.put("status", "success");
+        expBody.put("message", "Successfully! Record has been fetched.");
+        expBody.put("data", innerBodyOlusturMap());
+
+        return expBody;
+    }
 }
